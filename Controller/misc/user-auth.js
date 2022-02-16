4d6c2a05fc2506a38d1e87d8fd2a2a82eab5cf5e
Aryan Tiwari <l0stt3chi3@gmail.com>
2022-02-16 08:48:01 +0530

    commit

---
+++ Controller/misc/user-auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const { AppError } = require("./error");
+const user = require("../../Models/user");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (token) {
+      jwt.verify(token, process.env.jwt_sign, async (err, User) => {
+        if (err) {
+          return next(new AppError("token expired", 403));
+        } else if (User) {
+          const loggedStatus = await user.findOne({
+            blacklist: { $all: [token] },
+          });
+          if (loggedStatus) {
+            return next(new AppError("You have logged out buddy"));
+          }
+          req.user = User;
+          next();
+        } else {
+          return next(new AppError("auth failed", 404));
+        }
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- Routes/user.js
+++ Routes/user.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const Router = express.Router();
-const auth = require("../Controller/auth")
-const authenticate = require("../middleware/user-auth");
+const auth = require("../Controller/auth");
+const authenticate = require("../Controller/misc/user-auth");
 //GET requests
 
-
 Router.get("/verify/:token", auth.verify);
-Router.get("/user",authenticate, auth.detail);
+Router.get("/user", authenticate, auth.detail);
 Router.get("/logout", auth.logout);
 
 //POST requests
 Router.post("/signup", auth.signup);
-Router.post("/login" , auth.login);
+Router.post("/login", auth.login);
 Router.post("/refreshAuth", auth.refresh);
 Router.post("/forgot/:token", auth.changePassword);
 Router.post("/forgot", auth.forGet);
 Router.post("/reset", authenticate, auth.ResetDetails);
-
 
 module.exports = Router;
